fe97339bcee7d2cad3bdc573f6d689ef7fdc6c85
MedeirosJoel <joel.joao.medeiros@gmail.com>
2023-05-03 15:21:12 -0300

    tentativa de organizar num pacote

--- web_scrapper/__init__.py
+++ web_scrapper/__init__.py
@@ -1,4 +1,7 @@
 """
 Esse web scrapper obterá os metadados de https://repositorio.ufsc.br/handle/123456789/41812/recent-submissions
 """
-from get_all_links import initial_url, full_papper, get_all_links, 
+
+from .get_metadata import *
+from .get_all_links import *
+
